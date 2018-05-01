afe086a0e91e22d579309721b74548a75a68206a
Shubham <shubham.mahawar@qcin.org>
2018-05-01 14:07:24 +0530

    upload removed

--- upload.py
+++ upload.py
@@ -1,48 +1,48 @@
-import pyexcel
-import os
-from flask import request,jsonify
-from flask_restful import Resource
+# import pyexcel
+# import os
+# from flask import request,jsonify
+# from flask_restful import Resource
 
-class Upload(Resource):
-    def post(self):
-        try:
-            # print (request.files)
-            data = request.data
-            # print(data)
+# class Upload(Resource):
+#     def post(self):
+#         try:
+#             # print (request.files)
+#             data = request.data
+#             # print(data)
 
-            with open("qcisaved.csv","wb") as f:
-                f.write(data)
+#             with open("qcisaved.csv","wb") as f:
+#                 f.write(data)
             
-            r = pyexcel.SeriesReader("qcisaved.csv")
-            rows=r.to_records()
+#             r = pyexcel.SeriesReader("qcisaved.csv")
+#             rows=r.to_records()
             
 
-            # print(request.headers)
-            # # print(request.files["filename"])
-            # path = os.getcwd() + "" 
-            # filename=request.files['csv'].filename
-            # print(filename)
-            # r = pyexcel.SeriesReader(path)
-            # print(r)
+#             # print(request.headers)
+#             # # print(request.files["filename"])
+#             # path = os.getcwd() + "" 
+#             # filename=request.files['csv'].filename
+#             # print(filename)
+#             # r = pyexcel.SeriesReader(path)
+#             # print(r)
 
-        except Exception as e:
-            return jsonify({"success":False,"error":e.__str__()})
+#         except Exception as e:
+#             return jsonify({"success":False,"error":e.__str__()})
             
         
-        header = {
+#         header = {
                 
-                1: 'Vendor Name',
-                2: 'Invoice No',
-                3: 'Stock Id',
-                4: 'Date of purchase',
-                5: 'username',
-                6: 'category',
-                7: 'purchase value'
-                }
+#                 1: 'Vendor Name',
+#                 2: 'Invoice No',
+#                 3: 'Stock Id',
+#                 4: 'Date of purchase',
+#                 5: 'username',
+#                 6: 'category',
+#                 7: 'purchase value'
+#                 }
             
         
-        for n in rows:
-            row = dict(n)
+#         for n in rows:
+#             row = dict(n)
         
         
 
